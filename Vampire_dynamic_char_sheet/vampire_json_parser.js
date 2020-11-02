468d0b0b904d39f826624efd474fda9b0ca0675d
Mini-pen <cyrille.baudouin@gmail.com>
2020-11-02 10:37:36 +0100

    Update vampire sheet to last version

--- Vampire_dynamic_char_sheet/vampire_json_parser.js
+++ Vampire_dynamic_char_sheet/vampire_json_parser.js
@@ -3,7 +3,6 @@ function initPage() {
 		// Parse JSON string into object
 		//window.alert("before parse");
 		//window.alert("contenu du json :\n" + response);
-		console.error("contenu du json :\n" + response);
 		var persoJSON = null;
 		try {
            // null
@@ -21,7 +20,13 @@ function initPage() {
 			}
 			else {
 				if( (typeof attrValue) === 'number')
-				setNumberValue(attrName,attrValue, 8);
+				setNumberValue(attrName,attrValue, 20);
+			}
+			//special cases
+			if ( attrName == "illustration") {
+			var svgElemImage = document.getElementById("image_link");
+			svgElemImage.setAttribute('xlink:href', attrValue);
+			svgElemImage.setAttribute('sodipodi:absref', "./"+attrValue);
 			}
 		}		
 		//persoJSON.nom;
@@ -34,7 +39,7 @@ function setTextValue(labelName, textValue) {
 	//window.alert("Changing text of svgElem : " + labelName + " to text : " + textValue);
 	try {
            var svgElem = document.getElementById(labelName);
-		   if (svgElem != null) svgElem.textContent = textValue;
+		   if ((svgElem != null) && (typeof textValue === 'string') ) svgElem.textContent = textValue;
 		   //$('#'+labelName).text(textValue);
 		} catch (e) {
 		console.error("SVG elem pas trouvé :", e);
@@ -51,10 +56,15 @@ function setWillPool(value) {
 	setNumberValue("point_volonte", value, 10);
 }
 
+function setHealthLevel(value) {	
+	//window.alert("Changing text of svgElem : " + labelName + " to text : " + textValue);
+	setNumberValue("niveau_sante", value, 7);
+}
+
 function setNumberValue(baseNumberName, value, maxValue) {	
 	//window.alert("Changing value of svgElem : " + baseNumberName + " to value : " + value);
-	
-	for (var i = 0; i < maxValue; i++) {
+	if( ( (typeof value) === 'number') && ( (typeof maxValue) === 'number') ) {
+	for (var i = 0; i <= maxValue; i++) {
 		var svgElem = null;
 		try {
 			svgElem = document.getElementById(baseNumberName+i);
@@ -66,22 +76,64 @@ function setNumberValue(baseNumberName, value, maxValue) {
 		} catch (e) {
 			console.error("SVG elem pas trouvé :", e);
 		}				 
-	}		
+	}
+	}
 }
 
 function loadJSON(callback) {
 	var jsonFile = "dracula.json";
-	var xobj = new XMLHttpRequest();
+	var xobj = new XMLHttpRequest();	
 	xobj.overrideMimeType("application/json");	
 	window.alert("openning " + jsonFile);
-	xobj.open('GET', jsonFile, true); // Second element is the path to the file
-	xobj.onreadystatechange = function () {
-		if (xobj.readyState == 4) {
-			if (xobj.status == "200") {
-				// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-				callback(xobj.responseText);
+	try {
+		xobj.open('GET', jsonFile, true); // Second element is the path to the file
+		xobj.onreadystatechange = function () {
+			if (xobj.readyState == 4) {
+				if (xobj.status == "200") {
+					// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+					callback(xobj.responseText);
+				}
 			}
-		}
-	};
+		};
+	
 	xobj.send(null);  
+	} catch (e) {
+		console.error("Imposible de charger le fichier : ", e);
+		//openFile(dispFile);
+	}		
 }
+
+
+// function dispFile(contents) {
+  // document.getElementById('contents').innerHTML=contents
+// }
+
+// function clickElem(elem) {
+	// //Thx user1601638 on Stack Overflow (6/6/2018 - https://stackoverflow.com/questions/13405129/javascript-create-and-save-file )
+	// var eventMouse = document.createEvent("MouseEvents")
+	// eventMouse.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+	// elem.dispatchEvent(eventMouse)
+// }
+
+// function openFile(func) {
+	// readFile = function(e) {
+		// var file = e.target.files[0];
+		// if (!file) {
+			// return;
+		// }
+		// var reader = new FileReader();
+		// reader.onload = function(e) {
+			// var contents = e.target.result;
+			// fileInput.func(contents)
+			// document.body.removeChild(fileInput)
+		// }
+		// reader.readAsText(file)
+	// }
+	// fileInput = document.createElement("input")
+	// fileInput.type='file'
+	// fileInput.style.display='none'
+	// fileInput.onchange=readFile
+	// fileInput.func=func
+	// document.body.appendChild(fileInput)
+	// clickElem(fileInput)
+// }
