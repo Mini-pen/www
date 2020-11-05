a4828f11cd5c426132777577a6c334049cb9efeb
Mini-pen <cyrille.baudouin@gmail.com>
2020-11-05 14:37:20 +0100

    affichage milliseconds

--- SVG_lyrics_animator/svg_lyrics_parser.js
+++ SVG_lyrics_animator/svg_lyrics_parser.js
@@ -61,7 +61,7 @@ function computePrintableTime(time) {
   var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((time % (1000 * 60)) / 1000);
-  var milliseconds = Math.floor((time % (1000 * 60)) / 100);
+  var milliseconds = Math.floor((time % (1000 * 60)) / 1000000);
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
