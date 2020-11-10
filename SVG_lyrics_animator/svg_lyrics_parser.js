19c8dea08faf06a642b441443fecdcb5c5c9f519
Mini-pen <cyrille.baudouin@gmail.com>
2020-11-10 14:04:26 +0100

    remove window.alert

--- SVG_lyrics_animator/svg_lyrics_parser.js
+++ SVG_lyrics_animator/svg_lyrics_parser.js
@@ -41,8 +41,8 @@ var running = 0;
 function initPage() {
 	loadJSON(function(response) {
 		// Parse JSON string into object
-		//window.alert("before parse");
-		//window.alert("contenu du json :\n" + response);
+		//console.log("before parse");
+		//console.log("contenu du json :\n" + response);
 		console.log("contenu du json :\n",response);
 		var json_root = null;
 		try {
@@ -51,7 +51,7 @@ function initPage() {
 		} catch (e) {
 		console.error("Erreur lors du parse :", e); 
 		}
-		//window.alert("after parse :\n");
+		//console.log("after parse :\n");
 		
 		for (var key in json_root.Lyrics) {
 			//TODO populate timeEvent table
@@ -69,7 +69,7 @@ function initPage() {
 			// }
 		// }				
 		
-		window.alert("Init fini !");
+		console.log("Init fini !");
 	});
 		
 	svgGUI = getSvgElem("GUI");
@@ -461,7 +461,7 @@ function liberateTextObject(type,textObject) {
 }
 
 function setTextValue(labelName, textValue) {	
-	//window.alert("Changing text of svgElem : " + labelName + " to text : " + textValue);
+	//console.log("Changing text of svgElem : " + labelName + " to text : " + textValue);
 	try {
            var svgElem = document.getElementById(labelName);
 		   if (svgElem != null) svgElem.textContent = textValue;
@@ -472,17 +472,17 @@ function setTextValue(labelName, textValue) {
 }
 
 function setBloodPool(value) {	
-	//window.alert("Changing text of svgElem : " + labelName + " to text : " + textValue);
+	//console.log("Changing text of svgElem : " + labelName + " to text : " + textValue);
 	setNumberValue("point_sang", value, 20);
 }
 
 function setWillPool(value) {	
-	//window.alert("Changing text of svgElem : " + labelName + " to text : " + textValue);
+	//console.log("Changing text of svgElem : " + labelName + " to text : " + textValue);
 	setNumberValue("point_volonte", value, 10);
 }
 
 function setNumberValue(baseNumberName, value, maxValue) {	
-	//window.alert("Changing value of svgElem : " + baseNumberName + " to value : " + value);
+	//console.log("Changing value of svgElem : " + baseNumberName + " to value : " + value);
 	
 	for (var i = 0; i < maxValue; i++) {
 		var svgElem = null;
@@ -503,7 +503,7 @@ function loadJSON(callback) {
 	var jsonFile = "navire_de_sang.json";
 	var xobj = new XMLHttpRequest();
 	xobj.overrideMimeType("application/json");	
-	window.alert("openning " + jsonFile);
+	console.log("openning ", jsonFile);
 	xobj.open('GET', jsonFile, true); // Second element is the path to the file
 	xobj.onreadystatechange = function () {
 		if (xobj.readyState == 4) {
